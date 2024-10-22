6ccdeff5fbbabac9ec9883fab68f23cfa91aaded
Raniyaaaa <raniya182002@gmail.com>
2024-10-22 16:30:53 +0530

    using useContext

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,11 @@ import Card from "../UI/Card/Card"
 import classes from "./Home.module.css"
 
 
-const Home=()=>{
+const Home=(props)=>{
     return(
         <Card className={classes.home}>
             <h1>Welcome back!</h1>
         </Card>
-    )
-  
+    ) 
 }
 export default Home;
